760bb36536921a7c827c27b1dcae8fdde6ecf5e2
fonhorst <fonhorst@alipoov.nb@gmail.com>
2024-06-17 18:50:05 +0300

    ---

--- grader/api/endpoints/base_api.py
+++ grader/api/endpoints/base_api.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 
-from grader.settings import settings
-
 base_router = APIRouter()
 
 
 @base_router.get("/healthz/", tags=["base"], status_code=200)
 async def healthz():
-    return settings.app.APP_NAME
+    return "Grader"
