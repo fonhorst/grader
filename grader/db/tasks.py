84aeeef9b59f73bcd245088c20ea13b8f9ca37ba
fonhorst <alipoov.nb@gmail.com>
2025-04-02 20:00:35 +0300

    ---

--- grader/db/tasks.py
+++ grader/db/tasks.py
@@ -30,71 +30,7 @@ class ImpossibleTaskStatusTransition(Exception):
     pass
 
 
-class Base(DeclarativeBase):
-    type_annotation_map = {
-        uuid.UUID: UUID(as_uuid=True),
-        datetime.datetime: TIMESTAMP(timezone=True),
-        Dict[str, Any]: JSONB(),
-        Dict[str, Union[str, int, float, bool]]: JSONB()
-    }
-
-
-# Task statuses:
-# 0 - Created
-# 1 - Running
-# 2 - Finished
-# 3 - Failed
-# 4 - Cancelled
-class Task(Base):
-    __tablename__ = "tasks"
-
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
-
-    name: Mapped[str] = mapped_column(String(50), nullable=True)
-
-    requester: Mapped[str] = mapped_column(String(50), nullable=True)
-
-    student: Mapped[str] = mapped_column(String(50), nullable=True)
-
-    project: Mapped[str] = mapped_column(String(50), nullable=True)
-
-    tag: Mapped[str] = mapped_column(String(50), nullable=True)
-
-    task_type: Mapped[str] = mapped_column(String(16), nullable=False)
-
-    job_id: Mapped[str] = mapped_column(String(50), nullable=False)
-
-    priority: Mapped[float] = mapped_column(nullable=True, default=0.0)
-
-    parameters: Mapped[Dict[str, Any]] = mapped_column(nullable=True)
-
-    status: Mapped[str] = mapped_column(String(16), nullable=False)
-
-    status_updated_at: Mapped[datetime.datetime] = mapped_column(nullable=False)
-
-    submit_time: Mapped[datetime.datetime] = mapped_column(nullable=False)
-
-    end_time: Mapped[datetime.datetime] = mapped_column(nullable=True)
-
-    metrics: Mapped[Dict[str, Union[str, int, float, bool]]] = mapped_column(nullable=True)
-
-    # change 'lazy' option as you need on query time
-    # see: https://stackoverflow.com/questions/52249870/flask-sqlalchemy-change-lazy-in-different-situations
-    reason: Mapped["TaskFailReason"] = relationship(uselist=False, lazy='noload', cascade='all, delete')
-
-    def __repr__(self) -> str:
-        return f"Task(id={self.id!r}, name={self.name!r}, status={self.status!r})"
-
-
-class TaskFailReason(Base):
-    __tablename__ = "tasksfailreasons"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    task_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("tasks.id"), nullable=False)
-    error_message: Mapped[str] = mapped_column(nullable=False)
-    error_full: Mapped[str] = mapped_column(nullable=True)
-
-
-class TaskStatus(enum.Enum):
+class TaskStatus(str, enum.Enum):
     CREATED = 'created'
     RUNNING = 'running'
     FINISHED = 'finished'
@@ -122,13 +58,49 @@ class TaskStatus(enum.Enum):
         return False
 
 
-class TaskType(enum.Enum):
-    regular = "regular"
-    container = "container"
-    # spark on k8s as a batch task
-    spark = "spark"
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        uuid.UUID: UUID(as_uuid=True),
+        datetime.datetime: TIMESTAMP(timezone=True),
+        Dict[str, Any]: JSONB(),
+        Dict[str, Union[str, int, float, bool]]: JSONB()
+    }
 
 
+# Task statuses:
+# 0 - Created
+# 1 - Running
+# 2 - Finished
+# 3 - Failed
+# 4 - Cancelled
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+
+    user_id: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    tag: Mapped[str] = mapped_column(String(50), nullable=True)
+
+    attachment: Mapped[str] = mapped_column(String(50), nullable=True)
+
+    status: Mapped[str] = mapped_column(String(16), nullable=False)
+
+    status_updated_at: Mapped[datetime.datetime] = mapped_column(nullable=False)
+
+    submit_time: Mapped[datetime.datetime] = mapped_column(nullable=False)
+
+    end_time: Mapped[datetime.datetime] = mapped_column(nullable=True)
+
+    report: Mapped[str] = mapped_column(nullable=True)
+
+    def __repr__(self) -> str:
+        return f"Task(id={self.id!r}, user_id={self.user_id!r}, name={self.name!r}, status={self.status!r})"
+
+
+# TODO: revise the implementation of all functions below this line, refactor the code to synchronize them with the change in the classes above in this file
 def _standartize_datetime(dt: DateTimeType) -> datetime.datetime:
     if isinstance(dt, str):
         dt = datetime.datetime.fromisoformat(dt)
@@ -151,6 +123,97 @@ def handle_name_like(filters: list, param: Mapped[str], value: Optional[Union[st
     filters.append(expr)
 
 
+def create_task(
+    *,
+    uid: Optional[uuid.UUID] = None,
+    name: Optional[str] = None,
+    requester: Optional[str] = None,
+    student: Optional[str] = None,
+    project: Optional[str] = None,
+    tag: Optional[str] = None,
+    task_type: TaskType,
+    job_id: str,
+    priority: float = 0.0,
+    parameters: Dict[str, Any],
+    submit_time: Optional[datetime.datetime] = None
+) -> Task:
+
+    # TODO: revise the implementation
+    with SessionBuilder() as session:
+        dt = datetime.datetime.now()
+        task = Task(
+            id=uid or uuid.uuid4(),
+            name=name,
+            requester=requester,
+            student=student,
+            project=project,
+            tag=tag,
+            task_type=task_type.value,
+            job_id=job_id,
+            priority=priority,
+            parameters=parameters,
+            submit_time=submit_time or dt,
+            # TODO: unify with TaskStatus enum
+            status=TaskStatus.CREATED.value,
+            status_updated_at=submit_time or dt
+        )
+        session.add(task)
+        session.commit()
+
+    # TODO: should we read it again?
+    return get_task(uid)
+
+
+def get_task(task_id: Union[str, uuid.UUID], include_reason: bool = False) -> Task:
+    with SessionBuilder() as session:
+        if include_reason:
+            task = cast(
+                Task,
+                session.query(Task).filter(Task.id == task_id).options(joinedload(Task.reason, innerjoin=False)).one()
+            )
+        else:
+            task = cast(Task, session.get(Task, task_id))
+
+        session.query()
+
+    return task
+
+
+def update_task_status(task_id: Union[str, uuid.UUID], status: TaskStatus):
+    with SessionBuilder(bind=isolated_engine) as session:
+        with session.begin():
+            attempt = 0
+            max_retries = 3
+            while attempt < max_retries:
+                try:
+                    task = cast(Task, session.query(Task).filter(Task.id == task_id).with_for_update().one())
+
+                    curr_status = TaskStatus(task.status)
+
+                    if not TaskStatus.can_proceed(curr_status, status):
+                        raise ValueError(f"Cannot change status from {curr_status} to {status}")
+
+                    dt = datetime.datetime.now()
+                    task.status = status.value
+                    task.status_updated_at = dt
+
+                    if TaskStatus.is_terminal(status):
+                        task.end_time = dt
+
+                    break
+                except OperationalError:
+                    logger.error("Unsuccessful attempt to update task status "
+                                 "(task_uid=%s) due to operational exeception. "
+                                 "Retry %s of %s" % (task_id, attempt, max_retries), exc_info=True)
+                attempt += 1
+
+
+def delete_task(task_id: Union[str, uuid.UUID]):
+    with SessionBuilder() as session:
+        session.query(Task).filter(Task.id == task_id).delete()
+        session.commit()
+
+
 def list_tasks(
         uids: Optional[List[str]] = None,
         name: Optional[Union[str, List[str]]] = None,
@@ -163,6 +226,7 @@ def list_tasks(
         statusess: Optional[List[str]] = None,
         submit_time: Optional[Tuple[DateTimeType, DateTimeType]] = None,
         include_reason: bool = False) -> List[Task]:
+    # TODO: revise the implementation
     with SessionBuilder() as session:
         query = session.query(Task)
 
@@ -199,118 +263,9 @@ def list_tasks(
         return cast(List[Task], query.all())
 
 
-def get_task(task_id: Union[str, uuid.UUID], include_reason: bool = False) -> Task:
-    with SessionBuilder() as session:
-        if include_reason:
-            task = cast(
-                Task,
-                session.query(Task).filter(Task.id == task_id).options(joinedload(Task.reason, innerjoin=False)).one()
-            )
-        else:
-            task = cast(Task, session.get(Task, task_id))
-
-        session.query()
-
-    return task
-
-
-def create_task(
-    *,
-    uid: Optional[uuid.UUID] = None,
-    name: Optional[str] = None,
-    requester: Optional[str] = None,
-    student: Optional[str] = None,
-    project: Optional[str] = None,
-    tag: Optional[str] = None,
-    task_type: TaskType,
-    job_id: str,
-    priority: float = 0.0,
-    parameters: Dict[str, Any],
-    submit_time: Optional[datetime.datetime] = None
-) -> Task:
-
-    with SessionBuilder() as session:
-        dt = datetime.datetime.now()
-        task = Task(
-            id=uid or uuid.uuid4(),
-            name=name,
-            requester=requester,
-            student=student,
-            project=project,
-            tag=tag,
-            task_type=task_type.value,
-            job_id=job_id,
-            priority=priority,
-            parameters=parameters,
-            submit_time=submit_time or dt,
-            # TODO: unify with TaskStatus enum
-            status=TaskStatus.CREATED.value,
-            status_updated_at=submit_time or dt
-        )
-        session.add(task)
-        session.commit()
-
-    # TODO: should we read it again?
-    return get_task(uid)
-
-
-def update_task_status(task_id: Union[str, uuid.UUID], status: TaskStatus):
-    with SessionBuilder(bind=isolated_engine) as session:
-        with session.begin():
-            attempt = 0
-            max_retries = 3
-            while attempt < max_retries:
-                try:
-                    task = cast(Task, session.query(Task).filter(Task.id == task_id).with_for_update().one())
-
-                    curr_status = TaskStatus(task.status)
-
-                    if not TaskStatus.can_proceed(curr_status, status):
-                        raise ValueError(f"Cannot change status from {curr_status} to {status}")
-
-                    dt = datetime.datetime.now()
-                    task.status = status.value
-                    task.status_updated_at = dt
-
-                    if TaskStatus.is_terminal(status):
-                        task.end_time = dt
-
-                    break
-                except OperationalError:
-                    logger.error("Unsuccessful attempt to update task status "
-                                 "(task_uid=%s) due to operational exeception. "
-                                 "Retry %s of %s" % (task_id, attempt, max_retries), exc_info=True)
-                attempt += 1
-
-
-def report_task_metrics(task_id: Union[str, uuid.UUID], **kwargs):
-    with SessionBuilder() as session:
-        task: Task = session.get(Task, task_id)
-        d = dict(task.metrics) if task.metrics else dict()
-        d.update(kwargs)
-        task.metrics = d
-        session.commit()
-
 
 def delete_all_tasks():
     with SessionBuilder() as session:
         session.query(Task).delete()
         session.commit()
 
-
-def report_task_fail_reason(task_id: Union[str, uuid.UUID], error_message: str, error_full: Optional[str] = None):
-    with SessionBuilder() as session:
-        task = TaskFailReason(
-            task_id=task_id,
-            error_message=error_message,
-            error_full=error_full
-        )
-        session.add(task)
-        session.commit()
-
-
-def get_task_fail_reason(task_id: Union[str, uuid.UUID]) -> TaskFailReason:
-    with SessionBuilder() as session:
-        reason = cast(TaskFailReason, session.query(TaskFailReason).filter(TaskFailReason.task_id == task_id).one())
-
-    return reason
