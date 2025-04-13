79e67005bb85d191c1178d5e5d5643cb520a61ea
fonhorst <alipoov.nb@gmail.com>
2025-04-14 01:31:43 +0300

    fix the error of the database creation

--- docker/postgres/initdb/01-init.sql
+++ docker/postgres/initdb/01-init.sql
@@ -1,10 +1,16 @@
 -- Create the grader database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS grader
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TEMPLATE = template0
-    CONNECTION LIMIT = -1;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'grader') THEN
+        CREATE DATABASE grader
+            WITH 
+            OWNER = postgres
+            ENCODING = 'UTF8'
+            LC_COLLATE = 'en_US.utf8'
+            LC_CTYPE = 'en_US.utf8'
+            TEMPLATE = template0
+            CONNECTION LIMIT = -1;
+    END IF;
+END
+$$;
 
